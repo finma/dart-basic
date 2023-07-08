9d8a94cda8e942f4e3342d21f30b57d17a2e7be3
finma <firmanmaulana2403@gmail.com>
2023-07-08 22:17:01 +0700

    list

---
+++ bin/list.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  List<int> listInt = [];
+
+  print(listInt);
+
+  var listString = <String>[];
+
+  print(listString);
+
+  var names = <String>[];
+
+  names.add('finma');
+  names.add('maulana');
+
+  print(names);
+  print(names.length);
+}
