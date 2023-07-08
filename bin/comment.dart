1f7fd6588695e21a218812ff68e3458964942064
finma <firmanmaulana2403@gmail.com>
2023-07-08 21:35:25 +0700

    comment

---
+++ bin/comment.dart
@@ -0,0 +1,11 @@
+/// This is a documentation comment
+/// will be used to generate documentation
+void main(List<String> args) {
+  // This is a comment
+  print('Hello world!'); // This is also a comment
+
+  /**
+   * This is a multi-line comment
+   * This is also a multi-line comment
+   */
+}
