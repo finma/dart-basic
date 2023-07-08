a41c2a02753ba8a2345852c89f726563cdea4bb6
finma <firmanmaulana2403@gmail.com>
2023-07-08 22:31:43 +0700

    if statement

---
+++ bin/if.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var nilai = 80;
+
+  if (nilai > 80) {
+    print('A');
+  } else if (nilai > 70) {
+    print('B');
+  } else if (nilai > 60) {
+    print('C');
+  } else if (nilai > 50) {
+    print('D');
+  } else {
+    print('E');
+  }
+}
