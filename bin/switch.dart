ddb39e1c5f6a05e825185b50c621bf4facfd7947
finma <firmanmaulana2403@gmail.com>
2023-07-08 22:32:36 +0700

    switch

---
+++ bin/switch.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  var nilai = 'A';
+
+  switch (nilai) {
+    case 'A':
+      print('Nilai Anda A');
+      break;
+    case 'B':
+      print('Nilai Anda B');
+      break;
+    case 'C':
+      print('Nilai Anda C');
+      break;
+    case 'D':
+      print('Nilai Anda D');
+      break;
+    default:
+      print('Nilai Anda E');
+  }
+}
