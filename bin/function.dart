12df4cc7fb71330ccc5dbd5b668e33ef96d4a7b8
finma <firmanmaulana2403@gmail.com>
2023-07-09 09:22:34 +0700

    function

---
+++ bin/function.dart
@@ -0,0 +1,8 @@
+void sayHello(String firstName, String? lastName) {
+  print('Hello $firstName $lastName');
+}
+
+void main(List<String> args) {
+  sayHello('Firman', 'Maulana');
+  sayHello('Fin', 'Maulana');
+}
