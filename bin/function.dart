8a9ece9c70f7c847205f5ad1ab2c36603dfa76a6
finma <firmanmaulana2403@gmail.com>
2023-07-09 09:55:00 +0700

    function

--- bin/function.dart
+++ bin/function.dart
@@ -1,8 +1,31 @@
-void sayHello(String firstName, String? lastName) {
+void sayHello(String firstName, [String? lastName]) {
   print('Hello $firstName $lastName');
 }
 
+void sayHello2(
+    {required String firstName, String? lastName, String role = 'admin'}) {
+  print('Hello $firstName $lastName $role');
+}
+
+double sum(double num1, double num2) {
+  return num1 + num2;
+}
+
+double sum2(double num1, double num2) => num1 + num2;
+
 void main(List<String> args) {
   sayHello('Firman', 'Maulana');
-  sayHello('Fin', 'Maulana');
+  sayHello('Fin');
+
+  sayHello2(firstName: 'Firman', lastName: 'Maulana');
+  sayHello2(firstName: 'Firman', role: 'member');
+
+  print(sum(10, 20));
+  print(sum2(10, 20));
+
+  sayHello3('Firman');
+}
+
+void sayHello3(String firstName, [String? lastName]) {
+  print('Hello $firstName $lastName');
 }
